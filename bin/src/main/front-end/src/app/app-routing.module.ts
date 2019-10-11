087e6a82ebe78f86208b920c921f1b1ccde2ec17
Oscar Dominguez Ocaña <Oscar.Dominguez1@alu.uclm.es>
2019-10-11 13:11:59 +0200

    Directorios admin y routing creado

--- bin/src/main/front-end/src/app/app-routing.module.ts
+++ bin/src/main/front-end/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CitasComponent } from './citas/citas.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
     { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
     { path: 'citas', component: CitasComponent },
+	{ path: 'admin', component:	AdminComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
 
--- bin/src/main/front-end/src/app/app.module.ts
+++ bin/src/main/front-end/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CitasComponent } from './citas/citas.component';
+import { AdminComponent } from './admin/admin.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitasComponent,
+	AdminComponent,
     PageNotFoundComponent
   ],
   imports: [
