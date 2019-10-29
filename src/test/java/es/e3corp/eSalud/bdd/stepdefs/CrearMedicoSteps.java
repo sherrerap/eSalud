e4eea881e36aba5fe9cd06c0c684fe77399d9e6e
Sergio Herrera <sergio.herrera2@alu.uclm.es>
2019-10-29 13:44:05 +0100

    Añadido application.properties a src/test y inicio de cambio de versión

--- src/test/java/es/e3corp/eSalud/bdd/stepdefs/CrearMedicoSteps.java
+++ src/test/java/es/e3corp/eSalud/bdd/stepdefs/CrearMedicoSteps.java
@@ -1,49 +1,35 @@
 package es.e3corp.eSalud.bdd.stepdefs;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import cucumber.api.DataTable;
-import cucumber.api.java.Before;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import es.e3corp.eSalud.repository.UsuarioRepository;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 @SpringBootTest
 public class CrearMedicoSteps {
 
-    @Test
     @Given("^Se registra un usuario medico con los siguientes campos$")
-    public void se_registra_un_usuario_paciente_con_los_siguientes_campos(DataTable medicoDt) {
+    public void se_registra_un_usuario_medico_con_los_siguientes_campos(DataTable arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
         // E,K,V must be a scalar (String, Integer, Date, enum etc)
-
+        // throw new PendingException();
     }
 
-    @Test(expected = NullPointerException.class)
     @When("^el usuario medico guarda sus datos 'con todos los campos requeridos'$")
-    public void el_usuario_paciente_guarda_sus_datos_con_todos_los_campos_requeridos() {
-
+    public void el_usuario_medico_guarda_sus_datos_con_todos_los_campos_requeridos() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
     }
 
-    @Test
     @Then("^el resultado de guardar el usuario medico 'es correcto'$")
-    public void el_resultado_de_guardar_el_usuario_es_correcto() {
-        // Optional <Usuario> medicoinsertado =
-        // crepo.findUsuarioById(medicoUsuario.getId());
-        // String idmedicoinsertado = medicoinsertado.get().getId();
-        // assertEquals(medicoUsuario.getId(), idmedicoinsertado);
+    public void el_resultado_de_guardar_el_usuario_medico_es_correcto() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
     }
 
 }
--- src/test/java/es/e3corp/eSalud/bdd/stepdefs/Crear_UsuarioSteps.java
+++ src/test/java/es/e3corp/eSalud/bdd/stepdefs/Crear_UsuarioSteps.java
@@ -1,14 +1,13 @@
 package es.e3corp.eSalud.bdd.stepdefs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import es.e3corp.eSalud.repository.UsuarioRepository;
 import es.e3corp.eSalud.model.Usuario;
+
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 import org.springframework.data.mongodb.core.*;
 
@@ -17,35 +16,21 @@ public class Crear_UsuarioSteps {
     private MongoOperations mongoOperations;
     private Usuario u;
 
-    public Crear_UsuarioSteps(MongoOperations mongoOperations) {
-
-        this.mongoOperations = mongoOperations;
-
-    }
-
-    public Crear_UsuarioSteps() {
-
-    }
-
     @Given("^Se registra un usuario con los siguientes campos$")
     public void se_registra_un_usuario_con_losa_siguientes_campos(DataTable usuarioDt) {
-
         List<Usuario> usuarioList = usuarioDt.asList(Usuario.class);
-
         u = usuarioList.get(0);
-
     }
 
     @When("^el paciente guarda sus datos 'con todos los campos requeridos'$")
     public void el_paciente_guarda_sus_datos_con_todos_los_campos_requeridos() {
-
         this.mongoOperations.save(u);
-
+        
     }
 
     @Then("^el resultado de guardar el paciente 'es correcto'$")
     public void el_resultado_de_guardar_el_paciente_es_correcto() {
-
+        assertEquals(u.getId(), this.mongoOperations.findById(u.getId(), Usuario.class));
     }
 
 }
