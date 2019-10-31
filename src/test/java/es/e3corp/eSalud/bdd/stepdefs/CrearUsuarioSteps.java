fca472bf704b6f783f21ddc73380f0239fc6026a
HectorCespedes <Hector.Cespedes@alu.uclm.es>
2019-10-31 20:24:54 +0100

    Pruebas de login

--- src/test/java/es/e3corp/eSalud/bdd/stepdefs/CrearUsuarioSteps.java
+++ src/test/java/es/e3corp/eSalud/bdd/stepdefs/CrearUsuarioSteps.java
@@ -10,6 +10,7 @@ import es.e3corp.eSalud.repository.UsuarioRepository;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,12 +22,26 @@ public class CrearUsuarioSteps {
 	
 	 @Autowired
 	    private UsuarioRepository usuarioRepository;
-	    private Usuario u;
+	    private Usuario u= new Usuario();
 
 	@Given("se registra un usuario con los siguientes campos")
-	public void se_registra_un_usuario_paciente_con_los_siguientes_campos() {
-		 u = new Usuario("05706193P", "Hector", "Estoy hasta los huevos", "12343", "paciente", "", "", 845302345, "Miguelturra",
-	                "coijenwa", "emai");
+	public void se_registra_un_usuario_paciente_con_los_siguientes_campos(DataTable dataTable) {
+		 /*u = new Usuario("05706193P", "Hector", "Estoy hasta los huevos", "12343", "paciente", "", "", 845302345, "Miguelturra",
+	                "coijenwa", "emai");*/
+		
+		List<Map<String,String>> a = dataTable.asMaps(String.class,String.class);
+		
+		u.setDni(a.get(0).get("dni"));
+		u.setNombre(a.get(0).get("nombre"));
+		u.setApellidos(a.get(0).get("apellidos"));
+		u.setContraseña(a.get(0).get("contraseña"));
+		u.setRol(a.get(0).get("rol"));
+		u.setNumTelefono(Integer.parseInt(a.get(0).get("numTelefono")));
+		u.setEmail(a.get(0).get("email"));
+		u.setEspecialidad(a.get(0).get("especialidad"));
+		u.setLocalidad(a.get(0).get("localidad"));
+		u.setMedico(a.get(0).get("medico"));
+		u.setCentro(a.get(0).get("centro"));
 	    
 	}
 
@@ -39,6 +54,7 @@ public class CrearUsuarioSteps {
 	@Then("el resultado de guardar el usuario es correcto")
 	public void el_resultado_de_guardar_el_usuario_es_correcto() {
 	    assertEquals(u.getDni(), this.usuarioRepository.findByDniAndContraseña(u.getDni(), u.getContraseña()).getDni());
+	    this.usuarioRepository.deleteUsuario(u.getId());
 	}
 
 }
--- src/test/resources/features/Crear_Usuario.feature
+++ src/test/resources/features/Crear_Usuario.feature
@@ -3,5 +3,9 @@ Feature: Crear usuario
 	Scenario: Se podrá crear un usuario e introducirlo al sistema
 	
 		Given se registra un usuario con los siguientes campos
+		|	dni	    | nombre	  	| apellidos      | contraseña  | rol        | numTelefono    | email                 | localidad   | especialidad | medico			| centro  		|
+    |111111111| Miguel      | Peco Gomez     | passw1288   | medico     | 645612514      | usuariopM@ejemplo.com |	Ciudad real	| cabecera     |Juan Sanchez| Miguelturra |
+			
+		
 		When el usuario guarda sus datos con los campos requeridos
 		Then el resultado de guardar el usuario es correcto
