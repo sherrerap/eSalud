b97b395224089c51ad4b1d9b41b34f1d4e504164
FcoCrespo <Fco.Crespo@alu.uclm.es>
2019-11-05 10:23:30 +0100

    cambio en repository usuario

--- src/main/java/es/e3corp/eSalud/repository/UsuarioRepository.java
+++ src/main/java/es/e3corp/eSalud/repository/UsuarioRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository {
+	
     Optional<List<Usuario>> findAll();
 
     public void saveUsuario(Usuario usuario);
