f7b6a192a202ed8164306f2869b10b48770040e4
FcoCrespo <Fco.Crespo@alu.uclm.es>
2019-11-07 18:31:28 +0100

    se quita lo de md5 de la clase usuario

--- src/main/java/es/e3corp/eSalud/model/Usuario.java
+++ src/main/java/es/e3corp/eSalud/model/Usuario.java
@@ -53,23 +53,6 @@ public class Usuario {
         this.email = Utilidades.encriptar(email);
     }
     
-    public static String getMD5(String input) {
-    	try {
-	    	 MessageDigest md = MessageDigest.getInstance("MD5");
-	    	 byte[] messageDigest = md.digest(input.getBytes());
-	    	 BigInteger number = new BigInteger(1, messageDigest);
-	    	 String hashtext = number.toString(16);
-
-    	 while (hashtext.length() < 32) {
-    	 hashtext = "0" + hashtext;
-    	 }
-    	 return hashtext;
-    	 }
-    	 catch (NoSuchAlgorithmException e) {
-    	 throw new RuntimeException(e);
-    	 }
-    }
-    
     public Usuario() {
 
     }
