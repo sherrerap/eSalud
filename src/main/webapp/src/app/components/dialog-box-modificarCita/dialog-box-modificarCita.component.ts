ee6b8470fc40abdf13f245eafebb0fa3c202411e
Oscar Dominguez Ocaña <Oscar.Dominguez1@alu.uclm.es>
2019-11-07 17:20:10 +0100

    Cambios en ventanas de modificacion citas

--- src/main/webapp/src/app/components/dialog-box-modificarCita/dialog-box-modificarCita.component.ts
+++ src/main/webapp/src/app/components/dialog-box-modificarCita/dialog-box-modificarCita.component.ts
@@ -4,56 +4,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { CitasService } from '../../_services';
 
+export interface CitaData {
+  especialidad: string;
+  medico: string;
+  centro: string;
+  horacita: string;
+  fechacita: string;
+}
+
 @Component({
   selector: 'app-dialog-box-modificarCita',
   templateUrl: './dialog-box-modificarCita.component.html',
   styleUrls: ['./dialog-box-modificarCita.component.css']
 })
-export class DialogBoxModificarCita implements OnInit {
-    registerForm: FormGroup;
-    loading = false;
-    submitted = false;
-    error: string;
-    success: string;
+export class DialogBoxModificarCita{
+  action: string;
+  local_data: any;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private citaService: CitasService
-    ) { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogBoxModificarCita>,
+    //@Optional() is used to prevent error if no data is passed
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: CitaData) {
+    console.log(data);
+    this.local_data = { ...data };
+    this.action = this.local_data.action;
+  }
 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            idCita: ['', Validators.required],
-            idPaciente: ['', Validators.required],
-            fecha: ['', Validators.required],
-            tipo: ['', Validators.required],
-            centro: ['', Validators.required],
-            id_medico: ['', Validators.required],
-            hora: ['', Validators.required]
-        });
-    }
+  doAction() {
+    this.dialogRef.close({ event: this.action, data: this.local_data });
+  }
 
-    get f() { return this.registerForm.controls; }
+  closeDialog() {
+    this.dialogRef.close({ event: 'Cancel' });
+  }
 
-    onSubmit() {
-        this.submitted = true;
-        this.success = null;
-
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.citaService.update(this.registerForm.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    console.log("[CLIENTE] Médico registrado.")
-                    this.success = "Médico registrado correctamente."
-                },
-                error => {
-                    this.error = error;
-                    this.loading = false;
-                });
-    }
 }
