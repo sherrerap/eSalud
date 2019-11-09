db660b498a2d04e73b1088312bfcc82e836d28b8
FcoCrespo <Fco.Crespo@alu.uclm.es>
2019-11-10 00:23:03 +0100

    obtener usuario por rol en servicios comentado

--- src/main/java/es/e3corp/eSalud/Service/UsuarioServiceImpl.java
+++ src/main/java/es/e3corp/eSalud/Service/UsuarioServiceImpl.java
@@ -96,23 +96,23 @@ public class UsuarioServiceImpl implements UsuarioService {
 		return usersRol;
 	}
 
-  @Override
-  public List<Usuario> getUsersByRol(String rol) {
-    List<Usuario> usuarios = userRepository.findByRol(rol);
-    List<Usuario> listado = new ArrayList<>();
-    if (rol.equals("medico")) {
-      for (Usuario u : usuarios) {
-        Usuario temp = new Usuario();
-        temp.setNombre(u.getNombre());
-        temp.setApellidos(u.getApellidos());
-        temp.setEspecialidad(u.getEspecialidad());
-        temp.setCentro(u.getCentro());
-        listado.add(temp);
-      }
-    } else {
-      listado = usuarios;
-    }
-    return listado;
-  }
+//  @Override
+//  public List<Usuario> getUsersByRol(String rol) {
+//    List<Usuario> usuarios = userRepository.findByRol(rol);
+//    List<Usuario> listado = new ArrayList<>();
+//    if (rol.equals("medico")) {
+//      for (Usuario u : usuarios) {
+//        Usuario temp = new Usuario();
+//        temp.setNombre(u.getNombre());
+//        temp.setApellidos(u.getApellidos());
+//        temp.setEspecialidad(u.getEspecialidad());
+//        temp.setCentro(u.getCentro());
+//        listado.add(temp);
+//      }
+//    } else {
+//      listado = usuarios;
+//    }
+//    return listado;
+//  }
 
 }
