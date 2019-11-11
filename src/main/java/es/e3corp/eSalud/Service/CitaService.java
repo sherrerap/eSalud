9e6c2db522d09a220d12891aac210e4c562c620a
Oscar Dominguez Ocaña <Oscar.Dominguez1@alu.uclm.es>
2019-11-11 10:43:18 +0100

    CheckStyle sprint 3

--- src/main/java/es/e3corp/eSalud/Service/CitaService.java
+++ src/main/java/es/e3corp/eSalud/Service/CitaService.java
@@ -1,42 +1,76 @@
 package es.e3corp.eSalud.Service;
 
+import es.e3corp.eSalud.model.Cita;
 import java.util.List;
 
-import es.e3corp.eSalud.model.Cita;
+
 /**
-* @author e3corp
-*/
+ * Declaracion de la interfaz.
+ * 
+ * @author e3corp
+ */
+
 public interface CitaService {
   /**
-  * @author e3corp
-  */
+   * Método para obtener toda la lista de citas.
+   * 
+   * @author e3corp
+   */
   List<Cita> findAll();
+
   /**
-  * @author e3corp
-  */
+   * Método para encontrar cita por id.
+   * 
+   * @author e3corp
+   */
+
   Cita findByCitaId(String id);
+
   /**
-  * @author e3corp
-  */
+   * Método para guardar citas en la base de datos.
+   * 
+   * @author e3corp
+   */
+
   void saveCita(Cita cita);
+
   /**
-  * @author e3corp
-  */
+   * Método para actualizar cita.
+   * 
+   * @author e3corp
+   */
+
   void updateCita(Cita cita);
+
   /**
-  * @author e3corp
-  */
+   * Método para eliminar cita.
+   * 
+   * @author e3corp
+   */
+
   void deleteCita(String citaId);
+
   /**
-  * @author e3corp
-  */
+   * Método para buscar cita por id de paciente, fecha y hora.
+   * 
+   * @author e3corp
+   */
+
   Cita findCitaByPacienteMedicoFechaHora(String idPaciente, String idMedico, String fecha, String hora);
+
   /**
-  * @author e3corp
-  */
+   * Método para obtener las citas de un paciente en función de su id.
+   * 
+   * @author e3corp
+   */
+
   List<Cita> getCitasByPaciente(String dni);
+
   /**
-  * @author e3corp
-  */
+   * Método para obtener las citas de un médico en función de su id.
+   * 
+   * @author e3corp
+   */
+
   List<Cita> getCitasByMedico(String id);
 }
--- src/main/java/es/e3corp/eSalud/Service/CitaServiceImpl.java
+++ src/main/java/es/e3corp/eSalud/Service/CitaServiceImpl.java
@@ -1,79 +1,107 @@
 package es.e3corp.eSalud.Service;
 
+import es.e3corp.eSalud.exception.CitaNotFoundException;
+import es.e3corp.eSalud.model.Cita;
+import es.e3corp.eSalud.repository.CitasRepository;
 import java.util.List;
 import java.util.Optional;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import es.e3corp.eSalud.exception.CitaNotFoundException;
-import es.e3corp.eSalud.model.Cita;
-import es.e3corp.eSalud.repository.CitasRepository;
-import es.e3corp.eSalud.utilidades.Utilidades;
-
 @Service("CitaService")
 /**
-* @author e3corp
-*/
+ * @author e3corp
+ */
 @Transactional
 public class CitaServiceImpl implements CitaService {
   /**
-  * @author e3corp
-  */
+   * Variable de Log.
+   * 
+   * @author e3corp
+   */
   private static final Log LOG = LogFactory.getLog(CitaServiceImpl.class);
   /**
-  * @author e3corp
-  */
+   * Declaración del repositorio de citas.
+   * 
+   * @author e3corp
+   */
   private CitasRepository citaRepository;
+
   /**
-  * @author e3corp
-  */
+   * Constructor de la clase.
+   * 
+   * @author e3corp
+   */
   @Autowired
   public CitaServiceImpl(CitasRepository citaRepository) {
     this.citaRepository = citaRepository;
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para obtener todas las citas.
+   * 
+   * @author e3corp
+   */
   public List<Cita> findAll() {
     Optional<List<Cita>> cita = citaRepository.findAll();
     return cita.get();
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para obtener las citas en función del id.
+   * 
+   * @author e3corp
+   */
+
   public Cita findByCitaId(String citaId) {
     Optional<Cita> cita = citaRepository.findOne(citaId);
     if (cita.isPresent()) {
       LOG.debug(String.format("Read citaId '{}'", citaId));
       return cita.get();
-    } else
+    } else {
       throw new CitaNotFoundException(citaId);
+    }
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para guardar citas.
+   * 
+   * @author e3corp
+   */
+
   public void saveCita(Cita cita) {
     citaRepository.saveCita(cita);
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para actualizar citas.
+   * 
+   * @author e3corp
+   */
+
   public void updateCita(Cita cita) {
     citaRepository.updateCita(cita);
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para eliminar citas en función del id.
+   * 
+   * @author e3corp
+   */
+
   public void deleteCita(String citaId) {
     citaRepository.deleteCita(citaId);
   }
+
   /**
-  * @author e3corp
-  */
+   * Método para encontrar citas en función del id de paciente, fecha y hora.
+   * 
+   * @author e3corp
+   */
+
   public Cita findCitaByPacienteMedicoFechaHora(String idPaciente, String idMedico, String fecha, String hora) {
     LOG.info("[SERVER] Id paciente recibido: " + idPaciente);
     LOG.info("[SERVER] Id medico recibido: " + idMedico);
@@ -85,14 +113,14 @@ public class CitaServiceImpl implements CitaService {
 
   @Override
   public List<Cita> getCitasByPaciente(String dni) {
-	
+
     List<Cita> citas = citaRepository.findPaciente(dni);
     return citas;
   }
 
   @Override
   public List<Cita> getCitasByMedico(String id) {
-	
+
     List<Cita> citas = citaRepository.findMedico(id);
     return citas;
   }
