52689ac6939322e4c41397094be31d2a44020f8d
FcoCrespo <Fco.Crespo@alu.uclm.es>
2019-11-11 15:49:45 +0100

    controlar que cita en get se encriptan los datos que llegan

--- src/main/java/es/e3corp/eSalud/controller/CitaController.java
+++ src/main/java/es/e3corp/eSalud/controller/CitaController.java
@@ -45,9 +45,15 @@ public class CitaController {
 
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<Cita> getCitaFecha(@RequestParam(required = false) String paciente,
-      @RequestParam(required = false) String médico, @RequestParam(required = false) String fecha,
+      @RequestParam(required = false) String medico, @RequestParam(required = false) String fecha,
       @RequestParam(required = false) String hora) {
-    Cita cita = citasService.findCitaByPacienteMedicoFechaHora(paciente, médico, fecha, hora);
+	  
+	String pacienteEncriptado = Utilidades.encriptar(paciente);
+	String medicoEncriptado = Utilidades.encriptar(medico);
+	String fechaEncriptado = Utilidades.encriptar(fecha);
+	String horaEncriptado = Utilidades.encriptar(hora);
+	
+    Cita cita = citasService.findCitaByPacienteMedicoFechaHora(pacienteEncriptado, medicoEncriptado, fechaEncriptado, horaEncriptado);
     if (cita != null) {
       System.out.println("[SERVER] Cita encontrada: " + cita.getId());
       return ResponseEntity.ok(cita);
